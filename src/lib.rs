8ebb171a5b15ee08082cf6e7f0683a14b1b30cda
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-07-07 16:49:21 -0400

    feat: add order wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod expression;
+pub mod order;
 pub mod query;
---
+++ src/order.rs
@@ -0,0 +1,50 @@
+use surrealdb::sql::Idiom;
+
+pub struct Order {
+    pub order: Idiom,
+    pub random: bool,
+    pub collate: bool,
+    pub numeric: bool,
+    /// true if the direction is ascending
+    pub direction: bool,
+}
+
+impl Order {
+    pub fn asc<F>(field: F) -> Order
+    where
+        F: Into<Idiom>,
+    {
+        Order {
+            order: field.into(),
+            random: false,
+            collate: false,
+            numeric: false,
+            direction: true,
+        }
+    }
+
+    pub fn desc<F>(field: F) -> Order
+    where
+        F: Into<Idiom>,
+    {
+        Order {
+            order: field.into(),
+            random: false,
+            collate: false,
+            numeric: false,
+            direction: false,
+        }
+    }
+}
+
+impl From<Order> for surrealdb::sql::Order {
+    fn from(value: Order) -> Self {
+        Self {
+            order: value.order,
+            random: value.random,
+            collate: value.collate,
+            numeric: value.numeric,
+            direction: value.direction,
+        }
+    }
+}
